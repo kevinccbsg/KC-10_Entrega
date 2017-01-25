b5cc4c076e73fbb499633abb81e64821b32d4cc6
kevinccbsg <kevin2906@hotmail.com>
2017-01-26 00:28:08 +0100

    Arreglados fallos de linting en app.js y eliminado el debug del auth helper

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'use strict'
-const express       = require('express')
-const path          = require('path')
-const favicon       = require('serve-favicon')
-const logger        = require('morgan')
-const pug           = require('pug')
-const cookieParser  = require('cookie-parser')
-const bodyParser    = require('body-parser')
-const apiRouter     = require('./router/api')
-const passport      = require('passport')
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const pug = require('pug')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const apiRouter = require('./router/api')
+const passport = require('passport')
 // Express application
-const app           = express()
+const app = express()
 
 // mongoose helper connection
 require('./helpers/connectionHelper')
@@ -20,13 +20,13 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 app.use('/apiv1', apiRouter)
 
--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -4,15 +4,13 @@ const ExtractJwt = require('passport-jwt').ExtractJwt
 const passport = require('passport')
 const config = require('app-config')
 const Usuario = require('../models/Usuario')
-const debug = require('debug')('Nodepop:helpers:authHelper')
+// const debug = require('debug')('Nodepop:helpers:authHelper')
 
 let opts = config.config.optsAuth
 opts.jwtFromRequest = ExtractJwt.fromAuthHeader()
 
 passport.use(new JwtStrategy(opts, (jwtPayload, done) => {
   Usuario.findOne({ id: jwtPayload.sub }, (err, user) => {
-    debug(err)
-    debug(user)
     if (err) return done(err, false)
     if (!user) return done(null, false)
 
