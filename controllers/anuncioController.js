f8860ea1e5e167e56c6a9e01a02d380bf1baf790
kevinccbsg <kevin2906@hotmail.com>
2017-01-27 00:31:56 +0100

    Añadidos los parámetros query al controlador

--- controllers/anuncioController.js
+++ controllers/anuncioController.js
@@ -5,5 +5,12 @@ const helper = require('../helpers/responseHelper')
 
 module.exports.anunciosList = (req, res) => {
   debug('anunciosList')
+  let tag = req.query.tag
+  let nombre = req.query.nombre
+  let precio = req.query.precio
+  let start = req.query.start
+  let limit = req.query.limit
+  let sort = req.query.sort
+  let includeTotal = req.query.includeTotal
   helper.response(res, true, 'Listado', 200)
 }
--- models/Anuncio.js
+++ models/Anuncio.js
@@ -9,6 +9,15 @@ let anuncioSchema = new mongoose.Schema({
   tags: [String]
 })
 
+anuncioSchema.statics.list = (filter, limit, skip, fields, sort, callback) => {
+  const query = Anuncio.find(filter)
+  query.limit(limit)
+  query.skip(skip)
+  query.select(fields)
+  query.sort(sort)
+  query.exec(callback)
+}
+
 let Anuncio = mongoose.model('anuncio', anuncioSchema)
 
 module.exports = Anuncio
